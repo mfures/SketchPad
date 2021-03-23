adac642db7751b92b6038690991e51259fbb3e26
Matej <matej.fures0@gmail.com>
2021-03-23 13:05:52 +0100

    Update AModel.java
    Updated some parameters

--- SketchPad2/src/main/java/hr/fer/zemris/diprad/recognition/models/AModel.java
+++ SketchPad2/src/main/java/hr/fer/zemris/diprad/recognition/models/AModel.java
@@ -15,7 +15,7 @@ public class AModel {
 
 		if (!(bp1.size() == 3 && bp2.size() == 2)) {
 			if (!(bp2.size() == 3 && bp1.size() == 2)) {
-				System.out.println("Bad sizes: " + bp1.size() + " " + bp2.size());
+				// System.out.println("Bad sizes: " + bp1.size() + " " + bp2.size());
 				return false;
 			}
 
@@ -30,28 +30,28 @@ public class AModel {
 
 		List<Line> lines = LineModel.linesInPoints(bmw1.getBm().getPoints(), bp1, bmw1);
 		if (lines.size() != 2) {
-			System.out.println("Bad lines");
+			// System.out.println("Bad lines");
 			return false;
 		}
 
 		Line l1 = LineModel.recognize(bmw2);
 		if (l1 == null) {
-			System.out.println("Not a line");
+			// System.out.println("Not a line");
 			return false;
 		}
 
 		if (l1.getType() != LineType.HORIZONTAL) {
-			System.out.println("Not horizontal");
+			// System.out.println("Not horizontal");
 			return false;
 		}
 
 		Line l01 = lines.get(0);
 		Line l02 = lines.get(1);
-		double maxLengthRatio = 1.3;
+		double maxLengthRatio = 1.5;
 		double minLengthRatio = 1 / maxLengthRatio;
 		double lengthRatio = l01.length() / l02.length();
 		if (lengthRatio > maxLengthRatio || lengthRatio < minLengthRatio) {
-			System.out.println("Bad length ratio: " + lengthRatio);
+			// System.out.println("Bad length ratio: " + lengthRatio);
 			return false;
 		}
 
@@ -61,18 +61,18 @@ public class AModel {
 			l02 = tmp;
 		}
 		if (l01.getSlope() <= -10 || l01.getSlope() > -1) {
-			System.out.println("Bad l01 slope: " + l01.getSlope());
+			// System.out.println("Bad l01 slope: " + l01.getSlope());
 			return false;
 		}
 		if (l02.getSlope() >= 10 || l02.getSlope() < 1) {
-			System.out.println("Bad l02 slope: " + l02.getSlope());
+			// System.out.println("Bad l02 slope: " + l02.getSlope());
 			return false;
 		}
 
 		double baseLength = MyVector.norm(new Point(l01.getMinX(), l01.getMaxY()),
 				new Point(l02.getMaxX(), l02.getMaxY()));
 		if (l1.length() / baseLength > 2 || l1.length() / baseLength < 0.5) {
-			System.out.println("Bad base length ratio: " + l1.length() / baseLength);
+			// System.out.println("Bad base length ratio: " + l1.length() / baseLength);
 			return false;
 		}
 
@@ -80,16 +80,16 @@ public class AModel {
 		double targetX2 = l02.forY(l1.getAverageY());
 		double dist = targetX2 - targetX1;
 		if (dist <= 0) {
-			System.out.println("l1 to high");
+			// System.out.println("l1 to high");
 			return false;
 		}
 
-		if (!(l1.getMinX() < (targetX1 + 0.05 * dist) && (l1.getMaxX() > (targetX2 - 0.05 * dist)))) {
-			System.out.println("l1 doesnt cross both lines");
+		if (!(l1.getMinX() < (targetX1 + 0.20 * dist) && (l1.getMaxX() > (targetX2 - 0.20 * dist)))) {
+			// System.out.println("l1 doesnt cross both lines");
 			return false;
 		}
 		if (l1.length() / dist > 3) {
-			System.out.println("L1 too wide: " + l1.length() / dist);
+			// System.out.println("L1 too wide: " + l1.length() / dist);
 			return false;
 		}
 
